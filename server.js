e1ecbe369955bab763dd3095d6aeaea3863f99a9
titiksha008 <chughtitiksha@example.com>
2025-12-01 18:39:37 +0530

    url changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log(err));
 
