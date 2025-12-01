fb5df1247f4100654f32d153fa7660902542fe94
titiksha008 <chughtitiksha@example.com>
2025-12-01 18:49:25 +0530

    minor changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log(err));
 
